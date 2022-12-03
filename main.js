d32d34223ba13b5e85fb6ef9030108aa8de6e04f
Harsh Singh <sujeetsingh734321@gmail.com>
2022-12-03 19:21:17 +0530

    Changes

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ app.use("/api/v1/login", require("./routes/login"));
 app.use("/api/v1/profile", require("./routes/profile"));
 app.use("/api/v1/posts", require("./routes/posts"));
 app.use("/api/v1/categories", require("./routes/categories"));
+app.use("/api/v1/sub-categories", require("./routes/sub_categories"));
 app.use("/api/v1/purchase", require("./routes/purchase"));
+app.use("/api/v1/supplier", require("./routes/supplier"));
 
 app.listen(4000, () => {
   console.log("Example app listening on port http://localhost:4000");
---
+++ routes/supplier.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const mssql = require("mssql");
+const config = require("../config/sql.js");
+
+//Get all sub categories
+router.get("/", async (req, res) => {
+    try {
+        const pool = await mssql.connect(config);
+        const result = await pool
+            .request()
+            .query(`SELECT * FROM SupplierMaster`);
+        if (result.recordset.length === 0) {
+            res.status(400).send("Number not registered");
+        } else {
+            res.send(result.recordset);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//Add new Item
+
+module.exports = router;
